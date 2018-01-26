1c17c27495a103520b5342f69d8739d5afce87d0
GNT-GLOBAL\lam.nm <lam.nm@vn.gnt-global.com>
2018-01-26 10:09:40 +0700

    Spring ApplicationContext container

--- src/main/java/com/lamdevops/context/loadcontainer/MainApp.java
+++ src/main/java/com/lamdevops/context/loadcontainer/MainApp.java
@@ -6,11 +6,6 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class MainApp {
     public static void main(String[] args) {
-        String beansFile = "C:\\Users\\lam.nm\\Documents\\spring-core\\src\\main\\java\\com\\lamdevops\\context\\loadcontainer\\Beans.xml";
-        //ApplicationContext context = new FileSystemXmlApplicationContext(beansFile);
-        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 
-        HelloWorld hello = (HelloWorld) context.getBean("helloWorld");
-        System.out.println(hello.getMessage());
     }
 }
